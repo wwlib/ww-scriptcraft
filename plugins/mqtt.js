bbeae2ace491a4e48c99765d3d322e5c31b29d07
Andrew <andrew@worthwhilegames.org>
2016-10-03 17:53:41 -0400

    Sync'd with files at nodecraft
    * Pointing at tcp://test.mosquitto.org:1883

--- plugins/mqtt.js
+++ plugins/mqtt.js
@@ -1,17 +1,18 @@
 console.log('loading mqtt.js');
 
 var mqtt = require('sc-mqtt');
-var client = mqtt.client(); // default is localhost 1883
+var client = mqtt.client('tcp://test.mosquitto.org:1883','TestClient'); // default is localhost 1883
+var mqtt_topic = 'jmgx/all';
 client.connect();
 
-client.publish('minecraft',  // topic
+client.publish(mqtt_topic,  // topic
                'ready', // payload
                1,            // QoS (1 is send at least once)
                true );       // broker should retain message
 
 // events.on('block.BlockBreakEvent', function (listener, event){
 //   console.log('mqtt.js: sending minecraft/blockbreak');
-//   client.publish('minecraft',  // topic
+//   client.publish(mqtt_topic,  // topic
 //                  'blockbreak', // payload
 //                  1,            // QoS (1 is send at least once)
 //                  true );       // broker should retain message
@@ -24,10 +25,50 @@ client.publish('minecraft',  // topic
 
 events.blockBreak( function( event ) {
   console.log('mqtt.js: blockBreak');
-  console.log(event.player,  'You broke a block!' );
+  console.log(event.player);
+  console.log('You broke a block!' );
 
-  client.publish('minecraft',  // topic
-                 'blockBreak', // payload
+  var block = event.block;
+  console.log('Block:');
+  console.log(block);
+
+  var player_name = event.player.name;
+  console.log("Player name:");
+  console.log(player_name);
+
+  var block_type = String(block.type);
+  console.log("Block type:");
+  console.log(block_type);
+
+  console.log("Block Stringified");
+  try {
+    console.log(JSON.stringify(block));
+  } catch (error) {
+    console.log("Error: ");
+    console.log(error);
+  }
+
+  for (var p in event) {
+    console.log(p);
+  }
+
+  var message_obj = {
+    event: 'blockBreak',
+    player: player_name,
+    data: {
+      blocktype: block_type,
+      location: {x:block.getX(),y:block.getY(),z:block.getZ()}
+    }
+  }
+
+  console.log('Message Object:');
+  console.log(message_obj);
+
+  var message_obj_str = JSON.stringify(message_obj);
+  console.log(message_obj_str);
+
+  client.publish(mqtt_topic,  // topic
+                 message_obj_str, // payload
                  1,            // QoS (1 is send at least once)
                  true );
 });
@@ -35,7 +76,7 @@ events.blockBreak( function( event ) {
 events.playerJoin( function( event ) {
   console.log('mqtt.js: events.playerJoin', event.player);
   console.log( event.player, 'Welcome to the server!' );
-  client.publish('minecraft',  // topic
+  client.publish(mqtt_topic,  // topic
                  'playerJoin', // payload
                  1,            // QoS (1 is send at least once)
                  true );
